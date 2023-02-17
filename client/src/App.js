fb0d260bbabf31ebae755444308aac5ad55794ec
WorkinTaylan <acikgozta@gmail.com>
2023-02-17 16:14:16 +0300

    Kaydet, film kaydetmiyor. Düzeltme yapılmalı.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {Switch, Route, Link, useParams} from 'react-router-dom';
+import FilmListesi from './Filmler/FilmListesi';
+import Film from './Filmler/Film';
 import KaydedilenlerListesi from './Filmler/KaydedilenlerListesi';
 
 export default function App () {
@@ -11,7 +13,10 @@ export default function App () {
     const FilmleriAl = () => {
       axios
         .get('http://localhost:5001/api/filmler') // Burayı Postman'le çalışın
-        .then(response => {
+        .then(response=>{
+          const dataMv=response.data;
+          //console.log(data)
+        setMovieList(dataMv);
           // Bu kısmı log statementlarıyla çalışın
           // ve burdan gelen response'u 'movieList' e aktarın
         })
@@ -22,15 +27,26 @@ export default function App () {
     FilmleriAl();
   }, []);
 
-  const KaydedilenlerListesineEkle = id => {
-    // Burası esnek. Aynı filmin birden fazla kez "saved" e eklenmesini engelleyin
+  const KaydedilenlerListesineEkle = (id) => {
+       console.log('kaydete bastım', id)
+       const newSaved=[];
+       setSaved(id)
+       setSaved(...saved, id)// Burası esnek. Aynı filmin birden fazla kez "saved" e eklenmesini engelleyin
   };
 
   return (
     <div>
       <KaydedilenlerListesi list={[ /* Burası esnek */]} />
 
-      <div>Bu Div'i kendi Routelarınızla değiştirin</div>
+    <Switch>
+      <Route path={"/filmler/:id"}>
+        <Film saveCallBack={KaydedilenlerListesineEkle}>
+        </Film>
+      </Route>
+      <Route path='/'>
+        <FilmListesi movies={movieList}/>
+      </Route>
+    </Switch>
     </div>
   );
 }
